78db03eed0ebf848bd1de4d8f20bcc3808d0e6fe
jieunKim <zieunv@gmail.com>
2020-09-02 18:21:39 +0900

    ui: HomeReviewDetailVC layout

--- warmingUpProject_3/warmingUpProject_3/Scenes/Main/HomeReviewDetailViewController.swift
+++ warmingUpProject_3/warmingUpProject_3/Scenes/Main/HomeReviewDetailViewController.swift
@@ -118,7 +118,7 @@ UIViewController,ViewModelBindableType, UITextViewDelegate {
         }
             
         bookImg.snp.makeConstraints {
-            $0.leading.equalTo(bookView.snp.bottom)
+            $0.leading.equalTo(bookView.snp.leading).offset(15)
             $0.top.equalTo(bookView.snp.top).offset(14)
             $0.height.equalTo(60)
             $0.width.equalTo(46)
@@ -165,82 +165,44 @@ UIViewController,ViewModelBindableType, UITextViewDelegate {
         
         infoView.addSubview(timeLabel)
         infoView.addSubview(locationImg)
+        infoView.addSubview(locationLabel)
         infoView.addSubview(quotesImg)
         infoView.addSubview(opinionLabel)
         
+        timeLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(20)
+            $0.top.equalToSuperview().offset(5)
+        }
+        
+        locationImg.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(20)
+            $0.top.equalTo(timeLabel.snp.bottom).offset(4)
+            $0.width.equalTo(16)
+            $0.height.equalTo(16)
+        }
+        
+        locationLabel.snp.makeConstraints {
+            $0.leading.equalTo(locationImg.snp.trailing).offset(3)
+            $0.top.equalTo(timeLabel.snp.bottom).offset(4)
+        }
+        
+        quotesImg.snp.makeConstraints {
+            $0.width.equalTo(30)
+            $0.height.equalTo(24)
+            $0.leading.equalToSuperview().offset(20)
+            $0.top.equalTo(locationLabel.snp.bottom).offset(33)
+        }
+        
+        opinionLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(20)
+            $0.top.equalTo(quotesImg.snp.bottom).offset(20)
+            $0.trailing.equalToSuperview().offset(20)
+            $0.bottom.equalToSuperview().offset(20)
+        }
+        
         return infoView
     }()
-    
-    let bookCommentView: UIView = {
-        let commentView = UIView()
-        let textView = UITextView()
-        commentView.backgroundColor = ColorUtils.color247
-        
-        let seperLine = UILabel()
-        seperLine.backgroundColor = ColorUtils.color242
-        commentView.addSubview(seperLine)
-        
-        seperLine.snp.makeConstraints { (make) in
-            make.top.equalToSuperview().offset(-14)
-            make.width.equalToSuperview()
-            make.height.equalTo(1)
-            make.leading.equalToSuperview()
-            make.trailing.equalToSuperview()
-        }
-        
-        return commentView
-    }()
-    
-    let commentTextView: UITextView = {
-        let textView = UITextView()
-        textView.tag = 222
-        textView.textContainer.maximumNumberOfLines = 0
-        textView.textAlignment = .left
-        textView.backgroundColor = ColorUtils.color247
-        textView.attributedText = TextUtils.attributedPlaceholder(text: "이 책을 이 위치에 남기는 이유를 알려주세요.", letterSpacing: 0, aligment: .left)
-        return textView
-    }()
-    
-    let suggestCollectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        layout.sectionInset = .zero
-        layout.minimumLineSpacing = 7
-        layout.minimumInteritemSpacing = 0
-        layout.estimatedItemSize = CGSize(width: 10, height: 34)
-        layout.scrollDirection = .horizontal
-        let suggestListCollectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        suggestListCollectionView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 40)
-        suggestListCollectionView.backgroundColor = .white
-        suggestListCollectionView.showsHorizontalScrollIndicator = false
-        suggestListCollectionView.register(RoundCollectionCell.self, forCellWithReuseIdentifier: String(describing: RoundCollectionCell.self))
-        
-        return suggestListCollectionView
-    }()
-    
-    let lbTime: UILabel = {
-        let lbTime = UILabel()
-        lbTime.setTextWithLetterSpacing(text: "이 시간에 추천해요", letterSpacing: 0, lineHeight: 17, font: UIFont.systemFont(ofSize: 14, weight: .medium), color: ColorUtils.color34)
-        
-        return lbTime
-    }()
-    
-    let lbTag: UILabel = {
-        let lbTag = UILabel()
-        lbTag.setTextWithLetterSpacing(text: "태그", letterSpacing: 0, lineHeight: 17, font: UIFont.systemFont(ofSize: 14, weight: .medium), color: ColorUtils.color34)
-        let seperLine = UILabel()
-        seperLine.backgroundColor = ColorUtils.color242
-        lbTag.addSubview(seperLine)
-        
-        seperLine.snp.makeConstraints { (make) in
-            make.top.equalToSuperview().offset(-15)
-            make.width.equalToSuperview()
-            make.height.equalTo(1)
-            make.leading.equalToSuperview()
-            make.trailing.equalToSuperview()
-        }
-        return lbTag
-    }()
-    
+
     let tagCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.sectionInset = .zero
@@ -296,20 +258,11 @@ extension HomeReviewDetailViewController {
         scrollView.addSubview(writeView)
         mainView.addSubview(bookCoverView)
         writeView.addSubview(userTitleView)
+        
         writeView.addSubview(detailBookView)
-
         writeView.addSubview(bookInfoView)
-        writeView.addSubview(bookCommentView)
-        writeView.addSubview(commentTextView)
-        writeView.addSubview(lbTime)
-        writeView.addSubview(suggestCollectionView)
-        writeView.addSubview(lbTag)
         writeView.addSubview(tagCollectionView)
         
-//        scrollView.keyboardDismissMode = .onDrag
-        bookInfoView.delegate = self
-        commentTextView.delegate = self
-        //TODO: 스냅킷 데모에서 사용하던데 이유는?
         self.view.setNeedsUpdateConstraints()
         setLayout()
         
@@ -356,7 +309,6 @@ extension HomeReviewDetailViewController {
         
         
         userTitleView.snp.makeConstraints {
-//            $0.top.equalTo(colorListCollectionView.snp.bottom)
             $0.leading.equalTo(writeView.snp.leading)
             $0.trailing.equalTo(writeView.snp.trailing)
             $0.height.equalTo(55)
@@ -377,86 +329,14 @@ extension HomeReviewDetailViewController {
             $0.height.equalTo(40)
         }
         
-        bookCommentView.snp.makeConstraints {
-            $0.top.equalTo(bookInfoView.snp.bottom).offset(14)
-            $0.leading.equalTo(writeView.snp.leading).offset(20)
-            $0.trailing.equalTo(writeView.snp.trailing).offset(-20)
-            $0.height.equalTo(146)
-        }
-        
-        commentTextView.snp.makeConstraints { (make) in
-            make.top.equalTo(bookCommentView.snp.top).offset(16)
-            make.bottom.equalTo(bookCommentView.snp.bottom).offset(-16)
-            make.leading.equalTo(bookCommentView.snp.leading).offset(12)
-            make.trailing.equalTo(bookCommentView.snp.trailing).offset(-12)
-        }
-        
-        lbTime.snp.makeConstraints { (make) in
-            make.top.equalTo(bookCommentView.snp.bottom).offset(16)
-            make.leading.equalToSuperview().offset(20)
-            make.trailing.equalToSuperview().offset(-20)
-            make.height.equalTo(17)
-        }
-        
-        suggestCollectionView.snp.makeConstraints {
-            $0.top.equalTo(lbTime.snp.bottom)//.offset(14)
-            $0.leading.equalTo(writeView.snp.leading).offset(20)
-            $0.trailing.equalTo(writeView.snp.trailing).offset(-20)
-            $0.height.equalTo(63)
-        }
-        
-        lbTag.snp.makeConstraints { (make) in
-            make.top.equalTo(suggestCollectionView.snp.bottom).offset(15)
-            make.leading.equalToSuperview().offset(20)
-            make.trailing.equalToSuperview().offset(-20)
-            make.height.equalTo(17)
-        }
-        
         tagCollectionView.snp.makeConstraints {
-            $0.top.equalTo(lbTag.snp.bottom)
+            $0.top.equalTo(bookInfoView.snp.bottom)
             $0.leading.equalTo(writeView.snp.leading)
             $0.trailing.equalTo(Dimens.deviceWidth).offset(-30)
             $0.bottom.equalToSuperview()
         }
     }
-    
-    func setPlaceholder() {
-        if bookInfoView.text == "북커버에 남길 감상이나 문구를 적어주세요 (32자)" {
-            
-            bookInfoView.text = ""
-            let style = NSMutableParagraphStyle()
-            style.alignment = .center
-            bookInfoView.typingAttributes = [
-                NSAttributedString.Key.font : UIFont.systemFont(ofSize: 14, weight: .medium),
-                NSAttributedString.Key.foregroundColor : ColorUtils.color34,
-                NSAttributedString.Key.paragraphStyle: style
-            ]
-            
-        } else if bookInfoView.text == "" || bookInfoView.text.isEmpty {
-            
-            bookInfoView.font = UIFont.systemFont(ofSize: 14, weight: .medium)
-            bookInfoView.attributedText = TextUtils.attributedPlaceholder(text: "북커버에 남길 감상이나 문구를 적어주세요 (32자)", letterSpacing: -0.07)
-        }
-    }
-    
-    func setPlaceholder2() {
-        if commentTextView.text == "이 책을 이 위치에 남기는 이유를 알려주세요." {
-            
-            commentTextView.text = ""
-            let style = NSMutableParagraphStyle()
-            style.alignment = .center
-            commentTextView.typingAttributes = [
-                NSAttributedString.Key.font : UIFont.systemFont(ofSize: 14, weight: .light),
-                NSAttributedString.Key.foregroundColor : ColorUtils.color68,
-                NSAttributedString.Key.paragraphStyle: style
-            ]
-            
-        } else if commentTextView.text == "" || commentTextView.text.isEmpty {
-            
-            commentTextView.font = UIFont.systemFont(ofSize: 14, weight: .medium)
-            commentTextView.attributedText = TextUtils.attributedPlaceholder(text: "이 책을 이 위치에 남기는 이유를 알려주세요.", letterSpacing: 0)
-        }
-    }
+
 }
 
 
