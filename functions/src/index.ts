ba07c7299374e32f8dd1d1ce7e322373ca364e53
qbbang <qbbang@gmail.com>
2021-04-05 22:14:39 +0900

    add: firebase funtions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,25 +1,49 @@
-import * as functions from "firebase-functions"
-import * as admin from "firebase-admin" // firebaseStore
-admin.initializeApp()
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin"; // firebaseStore
+admin.initializeApp();
 
+export const firstAPI = functions.https.onRequest((_, res) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  res.send("응답으로 텍스트를 줍니당");
+});
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const firstAPI = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true})
-  response.send("응답으로 텍스트를 줍니당")
-})
+export const getBostonAreaWeather =
+functions.https.onRequest(async (request, response) => {
+  try {
+    const snapshot = await admin.firestore().doc("areas/greater-boston").get();
+    const data = snapshot.data();
+    response.send(data);
+  } catch (error) {
+    // Handle the error
+    console.log(error);
+    response.status(500).send(error);
+  }
+});
+
+export const onBostonWeatherUpdate = functions.firestore.document("cities-weather/boston-ma-us").onUpdate((change) => {
+  const after = change.after.data();
+  // 전송되는 데이터
+  const payload = {
+    data: {
+      temp: String(after.temp),
+      conditions: after.conditions,
+    },
+  };
+  return admin.messaging().sendToTopic("weather_bostom-ma-us", payload)
+      .catch((error) => {
+        console.error("");
+      });
+});
 
 export const getBostonWeather = functions.https.onRequest((request, response) => {
-    admin.firestore().doc("cities-weather/boston-ma-us").get() // get return 값이 Promise
-    .then(snapshot => {
-        const data = snapshot.data()
-        response.send(data)
-    })
-    p2.catch(error => {
-        // Handle the error 
-        console.log(error)
-        response.status(500).send(error)
-    })
-})
+  admin.firestore().doc("cities-weather/boston-ma-us").get() // get return 값이 Promise
+      .then((snapshot) => {
+        const data = snapshot.data();
+        response.send(data);
+      })
+      .catch((error) => {
+        // Handle the error
+        console.log(error);
+        response.status(500).send(error);
+      });
+});
