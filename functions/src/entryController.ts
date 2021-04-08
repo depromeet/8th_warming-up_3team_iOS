6bb52115dbcaddda87cadb35b988cfca0bf6c107
qbbang <qbbang@gmail.com>
2021-04-08 18:35:59 +0900

    deploy: /app API 추가됨
    - 포스트맨으로 JSON 바디 해도 fireStore에 반영이...안됨

--- functions/src/entryController.ts
+++ functions/src/entryController.ts
@@ -1,16 +1,15 @@
-// import {Response} from "express";
+// eslint-disable-next-line
+import {Response} from "express";
 import {db} from "./config/firebase";
 
-// 타입을 지정함
 type EntryType = {
   title: string,
   text: string,
-  coverImageUrl: string
 };
 
 type Request = {
   body: EntryType,
-  params: { entryId: string }
+  params: {entryId: string},
 };
 
 const addEntry = async (req: Request, res: Response) => {
@@ -35,4 +34,69 @@ const addEntry = async (req: Request, res: Response) => {
   }
 };
 
-export {addEntry};
+const getAllEntries = async (req: Request, res: Response) => {
+  try {
+    const allEntries: EntryType[] = [];
+    const querySnapshot = await db.collection("entries").get();
+    querySnapshot.forEach((doc: any) => allEntries.push(doc.data()));
+    return res.status(200).json(allEntries);
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+};
+
+const updateEntry = async (req: Request, res: Response) => {
+  const {
+    body: {text, title},
+    params: {entryId},
+  } = req;
+
+  try {
+    const entry = db.collection("entries").doc(entryId);
+    const currentData = (await entry.get()).data() || {};
+
+    const entryObject = {
+      title: title || currentData.title,
+      text: text || currentData.text,
+    };
+
+    await entry.set(entryObject).catch((error) => {
+      return res.status(400).json({
+        status: "error",
+        message: error.message,
+      });
+    });
+
+    return res.status(200).json({
+      status: "success",
+      message: "entry updated successfully",
+      data: entryObject,
+    });
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+};
+
+const deleteEntry = async (req: Request, res: Response) => {
+  const {entryId} = req.params;
+
+  try {
+    const entry = db.collection("entries").doc(entryId);
+
+    await entry.delete().catch((error) => {
+      return res.status(400).json({
+        status: "error",
+        message: error.message,
+      });
+    });
+
+    return res.status(200).json({
+      status: "success",
+      message: "entry deleted successfully",
+    });
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+};
+
+export {addEntry, getAllEntries, updateEntry, deleteEntry};
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,45 +1,14 @@
 import * as functions from "firebase-functions";
-import * as admin from "firebase-admin"; // firebaseStore
-import * as express from "express"; // REST API 프레임워크
-import {addEntry} from "./entryController";
+import * as express from "express";
+// eslint-disable-next-line
+import {addEntry, getAllEntries, updateEntry, deleteEntry} from "./entryController";
 
 const app = express();
-app.post("/entries", addEntry);
+
 app.get("/", (req, res) => res.status(200).send("Hey there!"));
+app.post("/entries", addEntry);
+app.get("/entries", getAllEntries);
+app.patch("/entries/:entryId", updateEntry);
+app.delete("/entries/:enrryId", deleteEntry);
+
 exports.app = functions.https.onRequest(app);
-
-const db = admin.firestore();
-export {admin, db};
-
-export const randomNumber =
-functions.https.onRequest((request, response) => {
-  console.log("request.body => ", request.body);
-  // 왜 안찍힐까?
-  // FIXME: body parser가 필요함!!
-  console.log("xxxxx => " + request.body.title);
-  const number = Math.round(Math.random() * 100);
-  console.log(number);
-  response.send(number.toString());
-});
-
-export const toTheQbbangBlog =
-functions.https.onRequest((request, response) => {
-  response.redirect("https://www.qbbang.me");
-});
-
-// firestore trigger for tracking activity
-export const logActivities =
-functions.firestore.document("/{collection}/{id}").onCreate((snap, context) => {
-  console.log(snap.data());
-  const collection = context.params.collection;
-  const id = context.params.id;
-  console.log(id);
-  const activities = admin.firestore().collection("activities");
-  if (collection === "requests" ) {
-    return activities.add({text: "a new tutorial request was added"});
-  }
-  if (collection === "users" ) {
-    return activities.add({text: "a new user signed up"});
-  }
-  return null;
-});
