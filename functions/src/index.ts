fda30f5d42b22e0695982cfc3a108ea0ca556b62
qbbang <qbbang@gmail.com>
2021-04-15 10:45:21 +0900

    check: 배포, 로컬 테스트 이상무!
    - .runtimeconfig.json이 중요함.
    firebase functions:config:get > .runtimeconfig.json

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,8 @@
 import * as functions from "firebase-functions";
 import * as express from "express";
+import * as swaggerUi from "swagger-ui-express";
+import * as swaggerDocument from "./config/swagger.json";
+
 // eslint-disable-next-line
 import {addEntry, getAllEntries, updateEntry, deleteEntry} from "./entryController";
 
@@ -10,5 +13,6 @@ app.post("/entries", addEntry);
 app.get("/entries", getAllEntries);
 app.patch("/entries/:entryId", updateEntry);
 app.delete("/entries/:enrryId", deleteEntry);
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 exports.app = functions.https.onRequest(app);
