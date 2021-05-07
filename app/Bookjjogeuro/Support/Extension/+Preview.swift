e548ed0c7c1664d2276f4acc54cbc33ea93f6838
qbbang <qbbang@gmail.com>
2021-05-07 11:46:07 +0900

    - 프로젝트 폴더명 변경

---
+++ app/Bookjjogeuro/Support/Extension/+Preview.swift
@@ -0,0 +1,33 @@
+//
+//  +Preview.swift
+//  Bookjjogeuro
+//
+//  Created by 이규현 on 2021/05/04.
+//  Copyright © 2021 team3. All rights reserved.
+//
+
+import UIKit
+
+#if DEBUG
+import SwiftUI
+
+@available(iOS 13, *)
+extension UIViewController {
+    private struct Preview: UIViewControllerRepresentable {
+        // this variable is used for injecting the current view controller
+        let viewController: UIViewController
+
+        func makeUIViewController(context: Context) -> UIViewController {
+            return viewController
+        }
+
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        }
+    }
+
+    func toPreview() -> some View {
+        // inject self (the current view controller) for the preview
+        Preview(viewController: self)
+    }
+}
+#endif
